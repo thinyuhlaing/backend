1adb9686a3ad90ef25c43edd0df4a2dd9734b5c5
thinyuhlaing <thinyuhlaing94@gmail.com>
2026-06-24 14:21:50 +0630

    chore(simple-products): remove

--- src/simple-products/interfaces/simple-product.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface SimpleProduct {
-  id: number;
-  name: string;
-  price: number;
-}
--- src/simple-products/schema/simple-products.schema.ts
+++
@@ -1,7 +0,0 @@
-import { numeric, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
-
-export const simpleProducts = pgTable('simple_products', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 255 }).notNull(),
-  price: numeric('price', { precision: 10, scale: 2 }).notNull(),
-});
